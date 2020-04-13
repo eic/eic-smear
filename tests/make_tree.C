2b3bac44de12e2b32a8eb2d9acb9109c3362bd48
Kolja Kauder <kkauder@gmail.com>
2020-04-13 09:10:10 -0400

    Improved path independence of wrapper macro

--- tests/make_tree.C
+++ tests/make_tree.C
@@ -1,10 +1,12 @@
-R__LOAD_LIBRARY(../build/libeicsmear.so);
+R__LOAD_LIBRARY(libeicsmear);
+#include "eicsmear/erhic/DisKinematics.h"
+#include <eicsmear/functions.h>
 
 void make_tree(std::string filstr){
-
   erhic::DisKinematics::BoundaryWarning=false;
  
   std::string dirstr = ".";
   std::string inputstr = dirstr + "/" + filstr;
   BuildTree(inputstr,dirstr);
 }
+
