fe914854b0e15e2120ceadc0fd347fb92c55b66b
Kolja Kauder <kkauder@gmail.com>
2026-09-24 12:23:32 -0400

    Catching _more_ HepMC problems where djangoh assigns a particle as its own daughter

--- src/erhic/TreeToHepMC.cxx
+++ src/erhic/TreeToHepMC.cxx
@@ -575,7 +575,12 @@ Long64_t TreeToHepMC(const std::string& inputFileName,
         if (inParticle->GetNChildren() != 0 ){
           cout << "Status is 1 but we have children?" << endl;
           inParticle->Print();
-          if (inParticle->GetChild1Index()== 1 && inParticle->GetChildNIndex() == 1){
+          // Djangoh has two known problems:
+          // I   K(I,1)  K(I,2)  K(I,3)  K(I,4)  K(I,5)
+          // 6      13        1       0       6       0         // self-parentage
+          // 8       1     2212       2       1       1         // child index is the beam electron
+          if ( inParticle->GetChild1Index()==t+1
+             || (inParticle->GetChild1Index()== 1 && inParticle->GetChildNIndex() == 1) ) {
             cout << "This looks like a known DJANGOH problem. Fixing the child indices to 0" << endl;
             inParticle->SetChild1Index( 0 );
             inParticle->SetChildNIndex( 0 );
